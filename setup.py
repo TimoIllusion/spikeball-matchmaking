cca8164aad0cd3a3d76318a2abad387e978c0186
Timo <timoleitri@aol.com>
2024-01-16 22:37:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="matchmaking",
+    version="0.1.0",
+    author="Timo Leitritz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
