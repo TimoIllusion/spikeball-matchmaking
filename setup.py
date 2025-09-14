9f86c63f0e66ef9579dc383829550bb59d78f0b8
Timo Leitritz <42964574+TimoIllusion@users.noreply.github.com>
2025-09-14 12:22:28 +0200

    Rework build system

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@ from Cython.Build import cythonize
 import numpy as np
 
 setuptools.setup(
-    name="matchmaking",
-    version="0.1.0",
-    author="Timo Leitritz",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    include_dirs=[np.get_include()],
     ext_modules=cythonize("matchmaking/interaction_calculator.pyx"),
+    include_dirs=[np.get_include()],
 )
